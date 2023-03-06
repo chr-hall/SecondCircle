7d6e6693d6207cb583f3fa9c2a570864cf3dc01c
chr-hall <117752515+chr-hall@users.noreply.github.com>
2023-03-06 15:13:58 +0100

    Add files via upload

---
+++ target/classes/data.sql
@@ -0,0 +1,165 @@
+    INSERT
+    INTO
+        "user"
+        (first_Name, Last_Name, User_Name, PassWord, Email_Address, Street_Address, zip_id)
+    VALUES
+        ('Adam', 'Andersson','Gurkan', 'banan', 'test@test.com', 'karlavägen', '3333');
+
+    INSERT
+    INTO
+        "user"
+         (first_Name, Last_Name, User_Name, PassWord, Email_Address, Street_Address, zip_id)
+    VALUES
+        ('Kalle', 'Karlsson','LillFlugan', 'melon', 'Kalle@Karlsson.com', 'Drottninggatan', '55555');
+
+   INSERT
+    INTO
+        "user"
+         (first_Name, Last_Name, User_Name, PassWord, Email_Address, Street_Address, zip_id)
+    VALUES
+        ('Stina', 'Olsson','Vårblomman', 'sommar', 'Stina@Olsson.com', 'Örtgatan', '32434');
+
+   INSERT
+    INTO
+        "user"
+         (first_Name, Last_Name, User_Name, PassWord, Email_Address, Street_Address, zip_id)
+    VALUES
+        ('Kal', 'P.Dal','BuffaloBill', 'ängel', 'Kal@PDal.com', 'Ystadvägen', '21464');
+
+   INSERT
+    INTO
+        "user"
+         (first_Name, Last_Name, User_Name, PassWord, Email_Address, Street_Address, zip_id)
+    VALUES
+        ('Fia', 'Med','Knuff', 'spelpjäs', 'Fia@Knuff.com', 'Gustavsvägen', '21555');
+
+           INSERT
+            INTO
+                "user"
+                 (first_Name, Last_Name, User_Name, PassWord, Email_Address, Street_Address, zip_id)
+            VALUES
+                ('Vytis', 'Clausen','Viking', 'tony', 'Viking@Clausen.com', 'Göteborgsallé', '23234');
+
+    INSERT
+    INTO
+        listing
+        (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+    VALUES
+        ('Converse all-star', 'Säljer converse all-star high top. Stl 42.', 100, '2023-01-22', 1, 4265, 49.0, 10, 'Rädda Barnen');
+
+
+ INSERT
+    INTO
+        listing
+        (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+    VALUES
+        ('Akustisk gitarr, Fender', 'Stålsträngad gitarr av märket Fender. Några repor, se bild.', 800, '2023-01-15', 4, 3680, 100.0, 2, 'Unicef');
+
+    INSERT
+       INTO
+           listing
+           (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+       VALUES
+           ('Boze-Hörlurar', 'Säljer pga lyxfällan', 999, '2023-01-13', 4, 2853, 150.0, 2, 'Röda Korset');
+INSERT
+       INTO
+           listing
+           (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+       VALUES
+           ('Nokia 3310', 'Riktig klenod från en svunnen tid. Samlarobjekt!', 499, '2023-01-15', 1, 5061, 59.00, 15, 'Röda Korset');
+
+      INSERT
+         INTO
+             listing
+             (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+         VALUES
+             ('Porslinskoppar', 'Fyra koppar av märket petunia.', 75, '2023-01-28', 3, 2491, 49.0, 10, 'WWF');
+
+  INSERT
+         INTO
+             listing
+             (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+         VALUES
+             ('Blandat Lego', 'Säljer min sons lego då han inte leker med det längre. Blandat city, riddare, ninjago.', 300, '2023-01-28', 3, 573, 29.0, 10, 'WWF');
+  INSERT
+         INTO
+             listing
+             (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+         VALUES
+             ('Samsung Smart-tv', 'En samsung smart-tv, aldrig använd.', 4999, '2023-01-29', 4, 2917, 400.0, 2, 'Cancerfonden');
+
+           INSERT
+                    INTO
+                        listing
+                        (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+                    VALUES
+                        ('iPhone S9', 'Låst, men något för den händige!', 2000, '2023-01-30', 4, 5061, 69.00, 2, 'SOS Barnbyar');
+  INSERT
+         INTO
+             listing
+             (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+         VALUES
+             ('Soffa', 'Fin tvåsitsig soffa, ikea', 200, '2023-01-30', 2, 128, 50.0, 10, 'Cancerfonden');
+
+ INSERT
+         INTO
+             listing
+             (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+         VALUES
+             ('Airpods', 'Lite dåligt ljud i högra, men funkar fint.', 349, '2022-12-30', 1, 2853, 49.0, 10, 'WWF');
+
+  INSERT
+                    INTO
+                        listing
+                        (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+                    VALUES
+                        ('iPhone 13', 'Oanvänd, fick i julklapp med föredrar android.', 4999, '2023-01-31', 2, 5061, 69.00, 10, 'UNHCR');
+
+ INSERT
+     INTO
+      listing
+      (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+      VALUES
+      ('Skinnhandskar', 'Äkta skinn, köpta på H&M.', 249, '2023-02-01', 5, 30, 49, 15, 'SOS Barnbyar');
+
+ INSERT
+     INTO
+      listing
+      (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+      VALUES
+      ('Barnstol', 'Grabben har växt ur sin stol, behöver inte ha den längre.', 79, '2023-02-01', 5, 384, 49, 15, 'SOS Barnbyar');
+
+       INSERT
+           INTO
+            listing
+            (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+            VALUES
+            ('Blandade frimärken', 'Min pappas samling, flera ovanliga.', 2049, '2023-02-01', 5, 2355, 100, 5, 'SOS Barnbyar');
+
+ INSERT
+    INTO
+             listing
+         (title, description, price, "date", seller_id, category_id, shipping_cost, donation_percent, donation_org)
+        VALUES
+         ('Pokémonkort', '350 kort, flera rares. Bl.a. en shiny Gyarados!', 1800, '2023-02-02', 6, 4221, 99, 8, 'Rädda Barnen');
+
+    INSERT
+    INTO
+      feedback
+      (feedback_message, listing_id, rating)
+    VALUES
+      ('Very good seller', 1, 10);
+
+  INSERT
+         INTO
+             purchase
+             (item_name, seller_id, buyer_id, price, donation_sum)
+         VALUES
+             ('Testsak', 1, 2, 15000, 74);
+
+   INSERT
+          INTO
+              purchase
+              (item_name, seller_id, buyer_id, price, donation_sum)
+          VALUES
+              ('testsak2', 1, 2, 374956, 83);
